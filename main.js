4323f4eaada5adc5915decaa400af5508261dbaa
Ricky-Rubin <owolabi1738@gmail.com>
2025-01-12 01:37:46 +0100

    Linked button to close book details input form

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const addBook = document.querySelector('#add-book');
 const form = document.querySelector('#form');
+const closeForm = document.querySelector('#close-form');
 
 function showForm() {
     form.style.display = 'block';
 }
 
+function cancelForm() {
+    form.style.display = 'none';
+}
+
 addBook.addEventListener('click', showForm);
+closeForm.addEventListener('click', cancelForm);
