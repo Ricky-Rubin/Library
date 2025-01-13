5b14fc9993380c621c8e1f0bb5c60cfd4dd56aef
Ricky-Rubin <owolabi1738@gmail.com>
2025-01-14 00:12:38 +0100

    Tweaked the DOM
    Created a function to handle appending a new div for each book into
    the div created for the books. Modified the div to show up with two
    buttons: one to toggle the read status and one to remove the book from
    the library array and the display area.

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 const addBook = document.querySelector('#add-book');
 const form = document.querySelector('#form');
 const closeForm = document.querySelector('#close-form');
+const mainLibrary = document.querySelector('#main-library')
+
+const library = [];
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = {
+        title: title,
+        author: author,
+        pages: pages,
+        read: read
+    }
+
+    library.push(book);
+    console.log(library);
+}
+
+function displayBook() {
+    form.style.display = 'none'
+
+    const bookDiv = document.createElement('div');
+    bookDiv.className = 'book-div'
+    bookDiv.innerHTML = `<div class='book-details'>${title.value} by ${author.value}, ${pages.value}</div>
+    <div class='book-btns'>
+        <button class='read-stat'>${read.value.toLowerCase === 'yes' ? 'Read: Yes' : 'Read: No'}</button>
+        <button class='remove-btn'>Remove</button>
+    </div>
+    `
+}
 
 function showForm() {
     form.style.display = 'block';
